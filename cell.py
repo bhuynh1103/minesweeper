e79fec4b9b5f4aa3694b3d4addd6608092aecad5
bhuynh1103 <37849140+bhuynh1103@users.noreply.github.com>
2018-05-01 14:30:42 -0500

    Add files via upload
    Game can now find out where the mouse is and if it is clicked or not. Cells can be revealed with a left click and flagged with a right click.

--- cell.py
+++ cell.py
@@ -8,19 +8,36 @@ class Cell:
         self.x = x
         self.y = y
         self.w = width
+        # Cells currently have 50% chance of having a bomb
         if randint(0, 2) == 1:
             self.bomb = True
         else:
             self.bomb = False
-        # self.bomb = True
-        self.revealed = True
+        self.revealed = False
+        self.flagged = False
 
     def draw(self, window):
         pygame.draw.rect(window, white, (self.x, self.y, self.w, self.w))
-        pygame.draw.rect(window, gray(40), (self.x, self.y, self.w, self.w), 1)
-
-        if self.revealed:
+        pygame.draw.rect(window, black, (self.x, self.y, self.w, self.w), 1)
+        
+        # Draws flag if the cell is flagged
+        if self.flagged and not self.revealed:
+            pygame.draw.rect(window, gray(100), (self.x, self.y, self.w, self.w))
+            pygame.draw.rect(window, black, (self.x, self.y, self.w, self.w), 1)
+        # If revealed draw its contents (bomb or no bomb)
+        elif self.revealed:
+            pygame.draw.rect(window, gray(200), (self.x, self.y, self.w, self.w))
+            pygame.draw.rect(window, black, (self.x, self.y, self.w, self.w), 1)            
             if self.bomb:
                 pygame.draw.ellipse(window, black, (self.x + (self.w - (self.w * .75)) - (self.w * .125),
                                                     self.y + (self.w - (self.w * .75)) - (self.w * .125),
                                                     self.w * .75, self.w * .75), 1)
+                
+    def has(self, mousePos):
+        mouseX = mousePos[0]
+        mouseY = mousePos[1]
+        return (mouseX > self.x and mouseX < self.x + self.w and 
+                mouseY > self.y and mouseY < self.y + self.w)
+    
+    def check(self):
+        pass
--- minesweeperRunner.py
+++ minesweeperRunner.py
@@ -16,18 +16,50 @@ for x in range(gridLength):
         grid[x].append(Cell(x * (boardSide // gridLength), y * (boardSide // gridLength), boardSide // gridLength))
 
 
+leftClicked = False
+rightClicked = False
 
 
 while True:
     screen.fill(white)
+    
+    mousePos = pygame.mouse.get_pos()
 
     for i in range(gridLength):
         for j in range(gridLength):
             grid[i][j].draw(screen)
+            
+            if grid[i][j].has(mousePos) and leftClicked:
+                grid[i][j].revealed = True
+            elif grid[i][j].has(mousePos) and rightClicked:
+                grid[i][j].flagged = True
 
     pygame.display.update()
+    
+    # DEBUG
+    # print(pygame.mouse.get_pressed())
 
     for event in pygame.event.get():
         if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
             pygame.quit()
             sys.exit()
+            
+        mouseStates = pygame.mouse.get_pressed()
+        
+        # Checks which mouse buttons are pressed
+        
+        # Left mouse button
+        if event.type == MOUSEBUTTONDOWN and mouseStates[0] == 1:
+            leftClicked = True
+        else:
+            leftClicked = False
+            
+        # Right mouse button
+        if event.type == MOUSEBUTTONDOWN and mouseStates[2] == 1:
+            rightClicked = True
+        else:
+            rightClicked = False        
+            
+            
+            
+        
